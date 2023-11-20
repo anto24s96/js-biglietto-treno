86204c2574d5bcdbe95ea2a0f497fd1d225021a5
anto24s96 <anto24s96@gmail.com>
2023-11-20 18:11:30 +0100

    FIX exercise completes

--- js/script.js
+++ js/script.js
@@ -6,29 +6,25 @@ console.log(km_desiderati);
 let età = prompt("Inserisci la tua età");
 console.log(età);
 
+let message;
+
 /* Creare una condizione per il 20% di sconto sui minorenni; */
 if(età < 18){
     let biglietto = ((0.21 * km_desiderati) * 20 / 100);
-    console.log(biglietto);
     
-    let message = "Hai diritto a uno sconto del 20%! Il prezzo del biglietto è: " + biglietto.toFixed(2);
-    console.log(message);
+    message = "Hai diritto a uno sconto del 20%! Il prezzo del biglietto è: " + biglietto.toFixed(2);
 }
 /* Creare una condizione per il 40% di sconto per gli over 65; */
 else if(età >= 65){
     let biglietto = ((0.21 * km_desiderati) * 40 / 100);
-    console.log(biglietto);
 
-    let message = "Hai diritto a uno sconto del 40%! Il prezzo del biglietto è: " + biglietto.toFixed(2);
-    console.log(message);
+    message = "Hai diritto a uno sconto del 40%! Il prezzo del biglietto è: " + biglietto.toFixed(2);
 }
 /* Creare la condizione senza sconto per età compresa tra 18-64; */
 else{
     let biglietto = (0.21 * km_desiderati);
-    console.log(biglietto);
 
-    let message = "Non hai diritto a nessuno sconto! Il prezzo del biglietto è: " + biglietto.toFixed(2);
-    console.log(message);   
+    message = "Non hai diritto a nessuno sconto! Il prezzo del biglietto è: " + biglietto.toFixed(2);
 }
 
-
+console.log(message);
